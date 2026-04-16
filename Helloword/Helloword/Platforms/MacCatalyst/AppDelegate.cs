e51284314eafe1564a6abceefc8f0e4a391d5c9b
Bhavadharini <bhavadharini@gmail.com>
2026-04-16 22:11:49 +0530

    Helloword project created

---
+++ Helloword/Helloword/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Helloword
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
