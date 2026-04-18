513aa346f1b088912db7024547f0dd92e58de345
Bhavadharini <bhavadharini@gmail.com>
2026-04-18 21:15:35 +0530

    LoginApp projected created

---
+++ LoginApp/LoginApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace LoginApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            string username = usernameEntry.Text;
+            string password = passwordEntry.Text;
+
+            if (username == "admin" && password == "1234")
+            {
+                await DisplayAlert("Success", "Login Successful!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Invalid Login", "OK");
+            }
+        }
+    }
+}
